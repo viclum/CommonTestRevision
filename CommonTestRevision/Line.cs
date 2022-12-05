a95a6ba3f7c1f911df3b32dd22d2c0e724c84966
Victor Lum <viclum@zoho.com>
2022-12-05 11:26:52 +0800

    Add project files.

---
+++ CommonTestRevision/Line.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTestRevision
+{
+    class Line
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public Line() { }
+        public Line(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double FindIntersectionX(Line l)
+        {
+            return (double)(B * l.C - l.B * C) / (A * l.B - B * l.A);
+        }
+
+        public double FindIntersectionY(Line l)
+        {
+            return 0;
+        }
+    }
+}
---
+++ CommonTestRevision/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CommonTestRevision;
+/*
+ : 2,3,5
+Enter the values of a,b,c of line 2 equation ax+by=c: 6,6,3
+The intersection point (x, y): (3.5, -4)
+*/
+Console.Write("Enter the values of a,b,c of line 1 equation ax+by=c: ");
+string line1Values = Console.ReadLine();
+string[] data = line1Values.Split(',');
+int a = Convert.ToInt32(data[0]);
+int b = Convert.ToInt32(data[1]);
+int c = Convert.ToInt32(data[2]);
+Line line1 = new Line(a, b, c);
+Console.WriteLine("{0} {1} {2}", line1.A, line1.B, line1.C);
+Console.Write("Enter the values of a,b,c of line 2 equation ax+by=c: ");
+string line2Values = Console.ReadLine();
+data = line2Values.Split(',');
+a = Convert.ToInt32(data[0]);
+b = Convert.ToInt32(data[1]);
+c = Convert.ToInt32(data[2]);
+Line line2 = new Line(a, b, c);
+Console.WriteLine("{0} {1} {2}", line2.A, line2.B, line2.C);
+Console.WriteLine("The intersection point (x, y): ({0}, {1})", 
+    line1.FindIntersectionX(line2), line1.FindIntersectionY(line2));
+
